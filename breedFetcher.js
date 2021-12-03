b028dd4746b5a434b190415bc476eec240810935
Noya Onggud <yanuo44@gmail.com>
2021-12-02 20:42:39 -0500

    index.js added, refactored  breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,30 @@
 const request = require('request');
-let content;
 let searchEndpointBreedURL = "https://api.thecatapi.com/v1/breeds/search?q=";
-const clientQuery = process.argv[2];
-let query = searchEndpointBreedURL += clientQuery;
 
-content = request(query, { json: true }, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  if (body[0] == undefined) {
-    console.log("Breed is not found.");
-  }  else {
-    console.log(body[0]);
-  }
-});
- 
+
+const fetchBreedDescription = (nameOfbreed, callback) =>{
+  let URL = searchEndpointBreedURL + nameOfbreed;
+  console.log(nameOfbreed, URL);
+  let description;
+
+  //request logic
+  request(URL, { json: true }, (err, data) => {
+    // console.log(data.body[0]);
+    if (err) {
+      return err;
+    }
+    if (data.body[0] === undefined) {
+      // console.log(data.body[0]);
+      console.log("Breed is not found.");
+    }  else {
+      description =  data.body[0];
+    }
+    callback(err, description);
+  });
+
+
+
+};
+  
+
+module.exports = { fetchBreedDescription };
